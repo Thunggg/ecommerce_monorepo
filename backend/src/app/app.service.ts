0cbc8af27e32f1c1ef14bb04d51e887476128fcf
Thunggg <thuannguyen20041028@gmail.com>
2026-05-17 22:44:14 +0700

    refactor: simplify AppService response and update environment variable validation exit code

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common'
-import { envConfig } from '../shared/config/validate'
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    const databaseUrl = envConfig.DATABASE_URL
-    return { message: databaseUrl }
+    return { message: 'Hello API' }
   }
 }
--- backend/src/shared/config/validate.ts
+++ backend/src/shared/config/validate.ts
@@ -1,12 +1,4 @@
-import fs from 'fs'
-import path from 'path'
 import { z } from 'zod'
-
-// kiểm tra file env có tồn tại hay ko
-if (!fs.existsSync(path.join(process.cwd() + '/backend', '.env'))) {
-  console.error('File .env not found')
-  process.exit(0)
-}
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
@@ -16,7 +8,7 @@ const envParsed = envSchema.safeParse(process.env)
 
 if (!envParsed.success) {
   console.error('❌ Invalid environment variables', envParsed.error.message)
-  process.exit(0)
+  process.exit(1)
 }
 
 export const envConfig = envParsed.data
