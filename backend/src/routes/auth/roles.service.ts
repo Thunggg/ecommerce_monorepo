6ab1457e4bf422f0b549efbcc31552b6d0c36be0
Thunggg <thuannguyen20041028@gmail.com>
2026-05-25 21:20:02 +0700

    fix: update getClientRoleId method to use raw SQL query and handle missing client role error

--- backend/src/routes/auth/roles.service.ts
+++ backend/src/routes/auth/roles.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../../shared/services/prisma.service'
 import { RoleName } from '../../shared/constants/role.constant'
+import { RoleType } from './auth.model'
 
 @Injectable()
 export class RolesService {
@@ -10,13 +11,15 @@ export class RolesService {
   async getClientRoleId(): Promise<number> {
     if (this.clientRoleId) return this.clientRoleId
 
-    const role = await this.prisma.role.findFirstOrThrow({
-      where: {
-        name: RoleName.CLIENT,
-      },
-    })
+    const role = await this.prisma.$queryRaw<
+      RoleType[]
+    >`SELECT * FROM "Role" WHERE name = ${RoleName.CLIENT} AND "deletedAt" IS NULL LIMIT 1`
 
-    this.clientRoleId = role.id
+    if (role.length === 0) {
+      throw new Error('Client role not found!')
+    }
+
+    this.clientRoleId = role[0].id
     return this.clientRoleId
   }
 }
