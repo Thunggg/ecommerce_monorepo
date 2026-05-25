1848f886b6cc6c4cb666746d2b5c38c5e8e226f1
Thunggg <thuannguyen20041028@gmail.com>
2026-05-25 20:15:41 +0700

    feat: enhance permission management in initial script by adding logic to update and delete permissions based on available routes

--- backend/src/initialScript/create-permisson.ts
+++ backend/src/initialScript/create-permisson.ts
@@ -11,7 +11,14 @@ async function bootstrap() {
   const router = server.router
   const prisma = new PrismaService()
 
-  const availableRoutes = router.stack
+  // Query vào DB để lấy ra danh sách các permission
+  const permissionInDB = await prisma.permission.findMany({
+    where: {
+      deletedAt: null,
+    },
+  })
+
+  const availableRoutes: { path: string; method: keyof typeof HTTPMethod; name: string }[] = router.stack
     .map((layer: any) => {
       if (layer.route) {
         const path = layer.route?.path
@@ -26,12 +33,60 @@ async function bootstrap() {
     })
     .filter((item: any) => item !== undefined)
 
-  const res = await prisma.permission.createMany({
-    data: availableRoutes,
+  // tạo biến để check xem có update hoặc create ko
+  let isUpdateOrDeletePermission = false
+
+  // Tạo object permissionInDB với key là [method-path]
+  const permissionInDBMap = permissionInDB.reduce<Record<string, (typeof permissionInDB)[number]>>((acc, item) => {
+    acc[`${item.path}-${item.method}`] = item
+
+    return acc
+  }, {})
+
+  // Tạo object permissionInDB với key là [method-path]
+  const availableRoutesMap = availableRoutes.reduce<Record<string, (typeof availableRoutes)[number]>>((acc, item) => {
+    acc[`${item.path}-${item.method}`] = item
+
+    return acc
+  }, {})
+
+  // Tìm permission trong db mà không tồn tại trong availableRoutes
+  const permissionToDelete = permissionInDB.filter((item) => {
+    return !availableRoutesMap[`${item.path}-${item.method}`]
   })
 
-  console.log(res)
+  if (permissionToDelete.length > 0) {
+    isUpdateOrDeletePermission = true
 
-  process.exit(0)
+    const resOfDeletePermission = await prisma.permission.deleteMany({
+      where: {
+        id: {
+          in: permissionToDelete.map((item) => item.id),
+        },
+      },
+    })
+
+    console.log('Delete permission: ', resOfDeletePermission.count)
+  }
+
+  // Tìm permission có trong availableRoutes mà hiện tại chưa có trong database
+  const permissionToUpdate = availableRoutes.filter((item) => {
+    return !permissionInDBMap[`${item.path}-${item.method}`]
+  })
+
+  if (permissionToUpdate.length > 0) {
+    isUpdateOrDeletePermission = true
+
+    const resOfDeletePermission = await prisma.permission.createMany({
+      data: permissionToUpdate,
+      skipDuplicates: true,
+    })
+
+    console.log('Create permission: ', resOfDeletePermission.count)
+  }
+
+  if (!isUpdateOrDeletePermission) {
+    console.log('No records need updating or deleting.')
+  }
 }
 bootstrap()
