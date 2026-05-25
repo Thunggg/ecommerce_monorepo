08882aa7a337b2a9c3e5b6d7f13f364ddf848ed3
Thunggg <thuannguyen20041028@gmail.com>
2026-05-25 19:13:24 +0700

    feat: implement permission creation in initial script by logging available routes and saving them to the database

--- backend/src/initialScript/create-permisson.ts
+++ backend/src/initialScript/create-permisson.ts
@@ -1,25 +1,37 @@
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from '../app/app.module'
+import { PrismaService } from '../shared/services/prisma.service'
+import { HTTPMethod } from '../shared/constants/role.constant'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(3000)
   const server = app.getHttpAdapter().getInstance()
   const router = server.router
+  const prisma = new PrismaService()
 
-  const availableRoutes: [] = router.stack
+  const availableRoutes = router.stack
     .map((layer: any) => {
       if (layer.route) {
+        const path = layer.route?.path
+        const method = layer.route?.stack[0].method.toUpperCase() as keyof typeof HTTPMethod
+
         return {
-          route: {
-            path: layer.route?.path,
-            method: layer.route?.stack[0].method,
-          },
+          path,
+          method,
+          name: method + ' ' + path,
         }
       }
     })
     .filter((item: any) => item !== undefined)
-  console.log(availableRoutes)
+
+  const res = await prisma.permission.createMany({
+    data: availableRoutes,
+  })
+
+  console.log(res)
+
+  process.exit(0)
 }
 bootstrap()
